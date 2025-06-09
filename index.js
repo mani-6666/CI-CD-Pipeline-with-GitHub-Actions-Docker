99816c398b49a1e6c4be4ec5949888179d580129
root <root@Mani>
2025-06-09 16:41:12 +0530

    CI/CD pipeline with GitHub Actions

---
+++ index.js
@@ -0,0 +1,5 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+app.get("/", (req, res) => res.send("Hello from Local CI/CD Pipeline!"));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
